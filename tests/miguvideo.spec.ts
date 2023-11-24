669de3e7612805d4b8c47a63484da9d246d741a6
Liu Kun <liukunup@163.com>
2023-11-25 01:37:32 +0800

    修改电话号码的读取器

--- tests/miguvideo.spec.ts
+++ tests/miguvideo.spec.ts
@@ -1,16 +1,16 @@
 import { test, expect } from '@playwright/test';
-import { Parameterized } from '../utils/parameterized';
+import { envPhoneNumberReader, csvPhoneNumberReader } from '../utils/pnReader';
 
-const inst = new Parameterized('../PhoneNumber.csv');
-for (const record of inst.getRecords()) {
+const pnReader = process.env.CI ? new envPhoneNumberReader() : new csvPhoneNumberReader('../PhoneNumber.csv');
+for (const pn of pnReader.getPhoneNumberList()) {
   // 咪咕视频
-  test(`miguvideo: ${record.pn}`, async ({ page }) => {
-    console.log(record.pn);
+  test(`miguvideo: ${pn}`, async ({ page }) => {
+    console.log(pn);
 
     await page.goto('https://www.miguvideo.com/');
     await page.locator('.user').first().click();
     await page.frameLocator('#login_frame').locator('.checkbox').first().click();
-    await page.frameLocator('#login_frame').getByRole('textbox', { name: '手机号' }).fill(record.pn);
+    await page.frameLocator('#login_frame').getByRole('textbox', { name: '手机号' }).fill(pn);
     await page.frameLocator('#login_frame').getByRole('button', { name: '获取验证码' }).click();
     await expect(page.frameLocator('#login_frame').getByRole('button', { name: '重新获取(59s)' })).toBeVisible();
   });
---
+++ utils/pnReader.ts
@@ -0,0 +1,61 @@
+import fs from 'fs';
+import path from 'path';
+import { parse } from 'csv-parse/sync';
+
+// 电话号码读取类
+abstract class PhoneNumberReader {
+    // 原始读取数据
+    protected rawData: string;
+
+    // 电话号码列表
+    protected pnList: string[];
+
+    // 获取原始读取数据
+    abstract getRawData(): string;
+
+    // 获取电话号码列表
+    abstract getPhoneNumberList(): string[];
+}
+
+// 从环境变量中读取电话号码
+export class envPhoneNumberReader extends PhoneNumberReader {
+
+    getRawData() {
+        this.rawData = process.env.PHONENUMBERS as string;
+        return this.rawData;
+    }
+
+    getPhoneNumberList(): string[] {
+        this.pnList = this.rawData?.split(",");
+        return this.pnList;
+    }
+}
+
+// 从CSV文件中读取电话号码
+export class csvPhoneNumberReader extends PhoneNumberReader {
+
+    private csvFile: string;
+
+    constructor(csvFile: string) {
+        super();
+        // 获取CSV文件名
+        this.csvFile = csvFile;
+    }
+
+    getRawData() {
+        this.rawData = fs.readFileSync(path.join(__dirname, this.csvFile), {encoding: "utf-8"});
+        return this.rawData;
+    }
+
+    getPhoneNumberList(): string[] {
+        const tmpList = parse(this.getRawData(), {
+            columns: true,
+            skip_empty_lines: true
+        });
+        this.pnList = tmpList.reduce((arr, obj) => {
+            arr.push(obj.pn);
+            return arr;
+        }, new Array<string>);
+        return this.pnList;
+    }
+}
